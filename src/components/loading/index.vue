91e71a469ae94754174c618ce413c59d0587254c
Gabriel Medina <gmedina.santos@gmail.com>
2018-08-15 21:46:41 -0400

    Add loader spinner

--- src/components/loading/index.vue
+++ src/components/loading/index.vue
@@ -1,8 +1,8 @@
 <template>
   <transition name="loading">
     <div v-show="loading" class="loading">
-      <span v-if="error">Ops! This is strange, sorry!</span>
-      <span v-else class="loading__text">Loading...</span>
+      <span v-if="error" class="loading__text">Ops! This is strange, sorry!</span>
+      <span v-else class="loading__spinner"></span>
     </div>
   </transition>
 </template>
@@ -20,7 +20,7 @@
 
 <style lang="scss" scoped>
   .loading {
-    position: absolute;
+    position: fixed;
     top: 0;
     left: 0;
     z-index: 1000;
@@ -31,13 +31,43 @@
     width: 100%;
     height: 100%;
     background: #2ECC71;
+  }
 
-    color: #fff;
-    font-size: 2em;
+  .loading__content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 
   .loading__text {
-    animation: loading 2s infinite alternate;
+    color: #fff;
+    font-size: 1.8em;
+  }
+
+  .loading__spinner {
+    position: relative;
+    display: inline-block;
+    width: 120px;
+    height: 4px;
+    border-radius: 20px;
+    background: rgba(#fff, .2);
+
+    &:after {
+      content: "";
+
+      position: absolute;
+      top: 0;
+      left: 0;
+
+      display: block;
+      width: 40px;
+      height: 4px;
+      border-radius: 20px;
+      background: rgba(#fff, .5);
+
+      animation: move 1s ease-in-out alternate infinite;
+    }
   }
 
   .loading-enter,
@@ -51,8 +81,9 @@
     transition: .5s ease;
   }
 
-  @keyframes loading {
-    0% { opacity: .4; }
-    100% { opacity: 1; }
+  @keyframes move {
+    0% { left: 0; }
+    50% { background: #fff; }
+    100% { left: calc(100% - 40px); }
   }
 </style>
